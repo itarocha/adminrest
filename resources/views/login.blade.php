025c4e427cb2ab5220b2c4befb781069afbaaedd
itarocha <itarocha@gmail.com>
2016-09-05 10:56:56 -0300

    Exemplo do Youtube

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.default')
 
 @section('content')
+<div>
+  <iframe height="150"
+     width="250"
+     src="http://www.youtube.com/embed/m5Jmh9JKnyQ"
+     frameborder="0"
+     allowfullscreen="">
+   </iframe>
+</div>
+
 <form method="GET" action="/login">
     <button class="button-primary">Login with Google</button>
 </form>
